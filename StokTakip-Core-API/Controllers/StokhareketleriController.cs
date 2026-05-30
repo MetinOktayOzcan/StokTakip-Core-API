c8bfb4aa8cc699fb9175cfe03b64f22cfdeb1822
Metin Oktay Özcan <118303061+MetinOktayOzcan@users.noreply.github.com>
2026-05-30 21:27:56 +0300

    feat: stok hareketi islem eklendi

--- StokTakip-Core-API/Controllers/StokhareketleriController.cs
+++ StokTakip-Core-API/Controllers/StokhareketleriController.cs
@@ -22,5 +22,53 @@ namespace StokTakip_Core_API.Controllers
 
             return Ok(StokHareketleri);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> StokHareketiEkle([FromBody] DTOs.StokhareketiDTO yeniHareketDTO)
+        {
+            var islemGorenUrun = await _context.Urunler.FindAsync(yeniHareketDTO.UrunID);
+
+            if (islemGorenUrun == null)
+            {
+                return NotFound(new { mesaj = "Hata! İşlem yapmak istediğiniz ürün veritabanında bulunamadı." });
+            }
+
+            if (yeniHareketDTO.IslemTuru.ToLower() == "giriş" || yeniHareketDTO.IslemTuru.ToLower() == "giris")
+            {
+                islemGorenUrun.StokAdedi += yeniHareketDTO.Miktar;
+            }
+            else if (yeniHareketDTO.IslemTuru.ToLower() == "çıkış" || yeniHareketDTO.IslemTuru.ToLower() == "cikis")
+            {
+                if (islemGorenUrun.StokAdedi < yeniHareketDTO.Miktar)
+                {
+                    return BadRequest(new { mesaj = $"Hata! Yetersiz Stok. Depoda sadece {islemGorenUrun.StokAdedi} adet ürün var." });
+                }
+                islemGorenUrun.StokAdedi -= yeniHareketDTO.Miktar;
+            }
+            else
+            {
+                return BadRequest(new { mesaj = "Geçersiz İşlem Türü! Lütfen sadece 'Giriş' veya 'Çıkış' yazınız." });
+            }
+
+            var eklenecekHareket = new Models.StokHareketleri
+            {
+                UrunID = yeniHareketDTO.UrunID,
+                IslemTuru = yeniHareketDTO.IslemTuru,
+                Miktar = yeniHareketDTO.Miktar,
+                Aciklama = yeniHareketDTO.Aciklama,
+                IslemTarihi = DateTime.Now
+            };
+
+            await _context.StokHareketleri.AddAsync(eklenecekHareket);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                mesaj = "Stok hareketi başarıyla işlendi ve ürünün güncel stoğu güncellendi.",
+                guncelStok = islemGorenUrun.StokAdedi,
+                hareket = eklenecekHareket
+            });
+        }
     }
 }
---
+++ StokTakip-Core-API/DTOs/StokhareketiDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace StokTakip_Core_API.DTOs
+{
+    public class StokhareketiDTO
+    {
+        public int UrunID { get; set; }
+        public string IslemTuru { get; set; }
+        public int Miktar { get; set; }
+        public string Aciklama { get; set; }
+    }
+}
