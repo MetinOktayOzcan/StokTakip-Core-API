751b447f831e57e59606d78632e14350b5a732df
Metin Oktay Özcan <118303061+MetinOktayOzcan@users.noreply.github.com>
2026-05-30 21:07:40 +0300

    fix: urun silme islemine guvenlik kontrolu eklendi ve stok hareketleri ef core koprusu kuruldu

--- StokTakip-Core-API/Controllers/UrunlerController.cs
+++ StokTakip-Core-API/Controllers/UrunlerController.cs
@@ -48,6 +48,49 @@ namespace StokTakip_Core_API.Controllers
             await _context.Urunler.AddAsync(eklenecekUrun);
             await _context.SaveChangesAsync();
             return Ok(new { Mesajlar = "OK!", urun = eklenecekUrun });
-        }   
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UrunGuncelle(int id, [FromBody] DTOs.UrunEkleDTO guncelUrunDTO)
+        {
+            var guncellenecekUrun = await _context.Urunler.FindAsync(id);
+
+            if (guncellenecekUrun == null)
+            {
+                return NotFound(new { mesaj = "Güncellenecek ürün bulunamadı!" }); // 404 Hatası dön
+            }
+            guncellenecekUrun.UrunAdi = guncelUrunDTO.UrunAdi;
+            guncellenecekUrun.BirimFiyati = guncelUrunDTO.BirimFiyat;
+            guncellenecekUrun.StokAdedi = guncelUrunDTO.StokMiktari;
+            guncellenecekUrun.KategoriID = guncelUrunDTO.KategoriID;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(new { mesaj = "OK!", urun = guncellenecekUrun });
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> UrunSil(int id)
+        {
+            bool stokHareketiVarMi = await _context.StokHareketleri.AnyAsync(s => s.UrunID == id);
+
+            if (stokHareketiVarMi)
+            {
+                return BadRequest(new { mesaj = "Hata! Bu ürünün stok geçmişi bulunduğu için sistemden kalıcı olarak silinemez." });
+            }
+
+            var silinecekUrun = await _context.Urunler.FindAsync(id);
+
+            if (silinecekUrun == null)
+            {
+                return NotFound(new { mesaj = "Silinmek istenen ürün yok" });
+            }
+
+            // 3. Ürünü veritabanından kaldır
+            _context.Urunler.Remove(silinecekUrun);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { mesaj = "Ürün sistemden silindi" });
+        }
+
     }
 }
